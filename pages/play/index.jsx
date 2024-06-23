b78d047aeebf37fa5acd199a0cdf9142445bec91
saji <skm.kiani@gmail.com>
2024-06-23 11:27:37 +0330

    add landing page

---
+++ pages/play/index.jsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import Image from "next/image";
+import Board from "@/components/board";
+import Score from "@/components/score";
+import styles from "@/styles/index.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.twenty48}>
+      <header>
+        <h1>2048</h1>
+        <Score />
+      </header>
+      <main>
+        <Board />
+      </main>
+      <div>
+        <h2 style={{ textAlign: 'center' }}>
+          🚀 <span>@0xjsd0k</span>
+        </h2>
+      </div>
+      
+    </div>
+  );
+}
