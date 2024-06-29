7c4a46d5814c4b6503b008069e6979130503d7a6
saji <skm.kiani@gmail.com>
2024-06-29 14:57:26 +0330

    using ref to prevent close

--- pages/play/index.jsx
+++ pages/play/index.jsx
@@ -1,9 +1,16 @@
 import Board from "@/components/board";
 import Score from "@/components/score";
 import styles from "@/styles/index.module.css";
-import { useEffect } from "react";
+import { createRef, useEffect } from "react";
 
 export default function Home() {
+
+  const boardRef = createRef()
+
+  useEffect(() => {
+    boardRef.current.focus()
+    boardRef.current?.click()
+  }, [])
 
   return (
     <div className={styles.twenty48}>
@@ -11,12 +18,12 @@ export default function Home() {
         <h1>2048</h1>
         <Score />
       </header>
-      <main>
+      <main ref={boardRef}>
         <Board />
       </main>
       <div>
         <h2 style={{ textAlign: 'center' }}>
-          🚀 <span>@0xjsd0k</span>
+          🚀 <span>@0xsjd0k</span>
         </h2>
       </div>
       
